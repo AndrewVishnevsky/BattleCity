40842369aab0e4582f83df7f584349952b0e4cf7
Andrew Vishnevsky <vishnev93@gmail.com>
2018-08-09 20:43:53 +0900

    prepare to shooting

--- BattleCity/Source/BattleCity/GameLogic/TankController.cpp
+++ BattleCity/Source/BattleCity/GameLogic/TankController.cpp
@@ -3,6 +3,7 @@
 
 #include "TankController.h"
 #include "Tank.h"
+#include "GameFramework/Actor.h"
 
 
 void ATankController::BeginPlay()
@@ -36,14 +37,31 @@ bool ATankController::GetSightRayHitLocation(FVector& OutHitLocation) const
 	auto ScreenLocation = FVector2D(ViewportSizeX*CrossHairXLocation, ViewportSizeY*CrossHairYLocation);
 	
 	//"de-project" the screen position
-	FVector CameraWorldPos;
-	FVector WorldDirection;
-	if (DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CameraWorldPos, WorldDirection))
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *WorldDirection.ToString());
-	}
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{	
 		//line trace along that look direction
+		GetLookVectorHitLocation(LookDirection, OutHitLocation);
+	}
 	return true;
+}
+
+bool ATankController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitDirection) const
+{
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection*LineTraceRange);
+	if (GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		StartLocation,
+		EndLocation,
+		ECollisionChannel::ECC_Visibility))
+				//Line trace succeeds))
+		{
+		HitResult.Location;
+		return true;
+		}
+	return false;
 }
 
 ATank * ATankController::GetContolledTank() const
@@ -59,10 +77,20 @@ void ATankController::AimTowardsCrosshair()
 	FVector HitLocation; // Out parameter
 	if (GetSightRayHitLocation(HitLocation))
 	{
-		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
+		UE_LOG(LogTemp, Warning, TEXT("Hit Direction: %s"), *HitLocation.ToString());
 		//Get world location if linetrace through crosshair
 		//if it hits the landscape
 			// TODO Tell controlled tank to aim at this point
 	}
 }
 
+bool ATankController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldPos;	//to be discarded
+	return DeprojectScreenPositionToWorld(
+		ScreenLocation.X,
+		ScreenLocation.Y, 
+		CameraWorldPos,
+		LookDirection
+		);
+}
