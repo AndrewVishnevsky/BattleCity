cd28958430c58d4390eefe1842d8284f50c631d2
Andrew Vishnevsky <vishnev93@gmail.com>
2018-08-13 23:26:46 +0900

    Rewiew Our Execution Flow

--- BattleCity/Source/BattleCity/Private/TankAimingComponent.cpp
+++ BattleCity/Source/BattleCity/Private/TankAimingComponent.cpp
@@ -4,6 +4,7 @@
 #include "TankAimingComponent.h"
 #include "TankBarrel.h"
 #include "Kismet/GameplayStatics.h"
+#include "GameFramework/Actor.h"
 #include "Components/StaticMeshComponent.h"
 
 
@@ -13,6 +14,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
@@ -41,6 +43,13 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelTowards(AimDirection);
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
+	}
+	else
+	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: No aim solve found"), Time);
 	}
 	//if no solution found do nothing
 
--- BattleCity/Source/BattleCity/Private/TankBarrel.cpp
+++ BattleCity/Source/BattleCity/Private/TankBarrel.cpp
@@ -1,12 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
+#include "GameFramework/Actor.h"
 
 
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
-	UE_LOG(LogTemp,Warning,TEXT ("Barrel-elevate() called at speed %s"),DegreesPerSecond)
+	auto Time=GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp,Warning,TEXT ("%f: Barrel-elevate() called at speed %s"), Time, RelativeSpeed)
 }
